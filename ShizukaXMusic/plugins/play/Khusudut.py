9fd71f13b603a61a56f76eee10d60789c87b026f
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-13 21:19:29 +0300

    Add files via upload

---
+++ ShizukaXMusic/plugins/play/Khusudut.py
@@ -0,0 +1,37 @@
+import os
+import random
+import requests
+from datetime import datetime
+from sys import version_info
+from time import time
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from ShizukaXMusic import app
+from ShizukaXMusic.utils.decorators.admins import AdminActual
+from strings import get_command
+
+
+
+disable_cut = []
+
+@app.on_message(filters.regex("^مايسترو$") & filters.group)
+async def zohary(client: Client, message: Message):
+    usr = await client.get_users(5809896714)
+    name = usr.first_name
+    user = await client.get_chat(5809896714)
+    Bio = user.bio
+    async for photo in client.iter_profile_photos(5809896714, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""**????**
+                    
+◉ 𝙽𝙰𝙼𝙴 »»[{usr.first_name}](https://t.me/{usr.username})
+◉ 𝚄𝚂𝙴𝚁 »» @{usr.username}
+◉ 𝙸𝙳 »» 5809896714
+◉ 𝙱𝙸𝙾 »» {Bio} """, 
+reply_markup=InlineKeyboardMarkup(
+          [                   
+            [                   
+              InlineKeyboardButton (name, url=f"https://t.me/{usr.username}")
+            ],               
+          ]              
+       )              
+    )
