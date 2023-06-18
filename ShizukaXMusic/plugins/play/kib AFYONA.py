b9726491bcd8acaf4b334fb61d91393b6a45a1bc
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-18 07:44:56 +0300

    Update kib AFYONA.py

--- ShizukaXMusic/plugins/play/kib AFYONA.py
+++ ShizukaXMusic/plugins/play/kib AFYONA.py
@@ -1,7 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
 from strings import get_command
-from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
 from ShizukaXMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
