da22e6a6e498dffc5b7152c9c6aaaf6ab596e54b
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-19 08:14:09 +0300

    Update aaegmut.py

--- ShizukaXMusic/plugins/play/aaegmut.py
+++ ShizukaXMusic/plugins/play/aaegmut.py
@@ -34,7 +34,7 @@ async def arbic(_, query: CallbackQuery):
                         "المطور", url=f"https://t.me/FHK_M5"),
                 ],[
                     InlineKeyboardButton(
-                        "رجـوع 🎶", callback_data=f"تفعيل"),                    
+                        "رجـوع 🎶", callback_data=f"gg"),                    
                 ],
             ]
         ),
