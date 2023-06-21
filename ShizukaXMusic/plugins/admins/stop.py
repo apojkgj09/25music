b6139fcd067c166780dc62741494e82837f62410
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-21 10:13:26 +0300

    Add files via upload

--- ShizukaXMusic/plugins/admins/stop.py
+++ ShizukaXMusic/plugins/admins/stop.py
@@ -1,21 +1,13 @@
-#
-# Copyright (C) 2021-2022 by TeamShizuka@Github, < https://github.com/TeamShizuka >.
-#
-# This file is part of < https://github.com/TeamShizuka/ShizukaXMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamShizuka/ShizukaXMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
-from ShizukaXMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
 from ShizukaXMusic import app
+
 from ShizukaXMusic.core.call import Shizuka
 from ShizukaXMusic.utils.database import set_loop
+from ShizukaXMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -23,36 +15,30 @@ STOP_COMMAND_chh = get_command("STOP_COMMAND_chh")
 
 
 @app.on_message(
-    command(STOP_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
+    filters.command(STOP_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
 )
+@app.on_message(
+    filters.command(["انهاء","ايقاف","لندا طفيها"],"") & filters.group & ~filters.edited & ~BANNED_USERS
+)
+@AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     await Shizuka.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
-        _["admin_9"].format(message.from_user.mention)
+        _["admin_9"].format(message.from_user.mention), disable_web_page_preview=True
     )
 
-
 @app.on_message(
-    command(STOP_COMMAND_chh)
-    & filters.channel
-    & ~filters.edited
-    & ~BANNED_USERS
+    filters.command(["انهاء","ايقاف","لندا طفيها"],"") & filters.channel & ~filters.edited & ~BANNED_USERS
 )
-async def stop_music_ch(cli, message: Message, _, chat_id):
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
     await Shizuka.stop_stream(chat_id)
     await set_loop(chat_id, 0)
-    if message.sender_chat:
-        mention = f'<a href=tg://user?id={message.chat.id}>{message.chat.title}</a>'
-    else:
-        mention = message.from_user.mention
     await message.reply_text(
-        _["admin_9"].format(mention)
+        _["admin_9"].format(message.from_user.mention), disable_web_page_preview=True
     )
