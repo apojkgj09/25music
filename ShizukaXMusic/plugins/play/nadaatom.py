9f628fddff7a7b2aaa9fb8afae8d5b0edd9a538e
المايسترو <128506664+apojkgj09@users.noreply.github.com>
2023-06-21 09:32:52 +0300

    Update nadaatom.py

--- ShizukaXMusic/plugins/play/nadaatom.py
+++ ShizukaXMusic/plugins/play/nadaatom.py
@@ -6,7 +6,7 @@ from pyrogram import filters, Client
 from ShizukaXMusic import app
 
 
-@app.on_message(filters.command(['نداء'], prefixes=""))
+@app.on_message(filters.command(['منشن'], prefixes=""))
 def call_random_member(client, message):
     chat_id = message.chat.id
     members = [
@@ -26,7 +26,7 @@ def call_random_member(client, message):
 
 
 
-@app.on_message(filters.command(['زوجني'], prefixes=""))
+@app.on_message(filters.command(['زوجني,ز'], prefixes=""))
 def call_random_member(client, message):
     chat_id = message.chat.id
     members = [
@@ -36,7 +36,7 @@ def call_random_member(client, message):
     random_member = random.choice(members)
     random_member_mention = f"[{random_member.user.first_name}](tg://user?id={random_member.user.id})"
     random_message = random.choice([
-        f"**•زوجتك ↓↓↓ ** {random_member_mention}",
+        f"**•زوجتك ↓↓↓ **\n {random_member_mention}",
         f"**•زوجتك ↓↓↓ ** \n {random_member_mention}"
     ])
     client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
